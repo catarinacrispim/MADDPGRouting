95daaff001d2aa70eba06de561b4a6d4266bed42
catarinacrispim <137402141+catarinacrispim@users.noreply.github.com>
2024-05-09 15:24:05 +0100

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,7 +1,14 @@
 import torch as T
 from torch import nn, tensor, rand, optim, device, cat, save, load, softmax
 import torch.nn.functional as F
+from torch_geometric.nn import SAGEConv, GCNConv, GATConv, GraphConv, NNConv
+import networkx as nx
+import numpy as np
+from torch_geometric.data import Data
+from torch_geometric.utils import from_networkx
 
+
+from environmental_variables import NEURAL_NETWORK, PATH_SIMULATION, SIM_NR
 
 
 class Agent:
@@ -11,9 +18,10 @@ class Agent:
         self.gamma = gamma
         self.tau = tau
         self.n_actions = n_actions
-        #self.agent_name = 'agent_joint_training_improved_strategy%s' % agent_idx
-        self.agent_name = 'agent_joint_training_gcritic_updated_fixed_tm_15_02_%s' % agent_idx
-        self.load_name = self.agent_name#'agent_joint_training_updated_vary_tm_fix_16_11_8.0%s' % agent_idx
+        #fa1 = fc1 # tau 0.0001
+        #fa2 = fc2 #
+        self.agent_name = 'agent_%s' %agent_idx
+        self.load_name = self.agent_name
         self.actor = ActorNetwork(alpha, actor_dims, fa1, fa2, n_actions, 
                                   chkpt_dir=chkpt_dir,  name=self.agent_name+'_actor', load_file=self.load_name+'_actor')
         self.critic = CriticNetwork(beta, critic_dims, 
@@ -28,12 +36,16 @@ class Agent:
                                             name=self.agent_name+'_target_critic', load_file=self.load_name+'_target_critic')
 
         self.update_network_parameters(tau=1)
-
+ 
     def choose_action(self, observation):
-        state = T.tensor([observation], dtype=T.float).to(self.actor.device)
+        observation_array = np.array([observation], dtype=np.float32)
+        #print("\nobservation array []: ", observation_array)
+        state = T.tensor(observation_array, dtype=T.float).to(self.actor.device)
+
         actions = self.actor.forward(state)
         noise = T.rand(self.n_actions).to(self.actor.device)
-        action = actions
+        action = actions ##
+        #action = actions + noise
         return action.detach().cpu().numpy()[0]
 
     def update_network_parameters(self, tau=None):
@@ -81,12 +93,22 @@ class CriticNetwork(nn.Module):
         super(CriticNetwork, self).__init__()
 
         self.file_name = f"{name}.sync"  # os.path.join(chkpt_dir, name)
-        self.chkpt_file = f'/content/drive/MyDrive/{self.file_name}'
-        self.load_file = f'/content/drive/MyDrive/{load_file}.sync'
+        self.chkpt_file = f'{PATH_SIMULATION}/agent_files{SIM_NR}/{self.file_name}'
+        self.load_file = f'{PATH_SIMULATION}/agent_files{SIM_NR}/{load_file}.sync'
 
+
+        #change accordingly
         self.fc1 = nn.Linear(input_dims + n_actions, fc1_dims).float()
-        # self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-        self.q = nn.Linear(fc1_dims, 1)
+        #self.fc1 = nn.Linear(input_dims + n_agents * n_actions, fc1_dims)
+        #self.fc2 = nn.Linear(fc1_dims, fc2_dims) ## hidden layer 1
+        #self.q = nn.Linear(fc1_dims, 1)
+
+        if NEURAL_NETWORK == "duelling_q_network":
+            self.q = nn.Linear(fc1_dims, 1)          #       
+            self.q_values = nn.Linear(fc1_dims, n_actions)      #advantage
+            self.output = nn.Linear(n_actions, 1)
+        elif NEURAL_NETWORK == "simple_q_network":
+            self.q = nn.Linear(fc1_dims, 1)                     #1 dimention output
 
         self.optimizer = optim.Adam(self.parameters(), lr=beta)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
@@ -94,10 +116,29 @@ class CriticNetwork(nn.Module):
         self.to(self.device)
 
     def forward(self, state, action):
-        x = F.relu(self.fc1(T.cat([state, action], dim=1)))
-        # x = F.relu(self.fc2(x))
-        q = self.q(x)
+        #change accordingly
+        if NEURAL_NETWORK == "duelling_q_network":
+            x = F.relu(self.fc1(T.cat([state, action], dim=1)))
+            #x = F.relu(self.fc2(x)) ##
+            value = self.q(x)   #output 1
+            
+            q_values = T.softmax(self.q_values(x), dim=1)  
+            average = T.mean(q_values, dim = 1, keepdim=True)
+            advantage_function = q_values - average
 
+            q = value + advantage_function         #q = value + (q_value - average q values)
+
+            #q = self.output(q)  ##verificar
+
+            q , _= T.max(q, dim=1, keepdim=True)
+
+        elif NEURAL_NETWORK == "simple_q_network":
+            x = F.relu(self.fc1(T.cat([state, action], dim=1)))
+            #x = F.relu(self.fc2(x)) ##
+            q = self.q(x)
+            #print("\n simple q network q: ", q)
+            #print("\n shape: ", q.shape)
+    
         return q
 
     def save_checkpoint(self):
@@ -113,11 +154,12 @@ class ActorNetwork(nn.Module):
         super(ActorNetwork, self).__init__()
 
         self.chkpt_file = f"{name}.sync"  # os.path.join(chkpt_dir, name)
-        self.chkpt_file = f'/content/drive/MyDrive/{self.chkpt_file}'
-        self.load_file = f'/content/drive/MyDrive/{load_file}.sync'
+        self.chkpt_file = f'{PATH_SIMULATION}/agent_files{SIM_NR}/{self.chkpt_file}'
+        self.load_file = f'{PATH_SIMULATION}/agent_files{SIM_NR}/{load_file}.sync'
 
+        #change accordingly
         self.fc1 = nn.Linear(input_dims, fc1_dims)
-        # self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        #self.fc2 = nn.Linear(fc1_dims, fc2_dims) ##
         self.pi = nn.Linear(fc1_dims, n_actions)
 
         self.optimizer = optim.Adam(self.parameters(), lr=alpha)
@@ -126,9 +168,12 @@ class ActorNetwork(nn.Module):
         self.to(self.device)
 
     def forward(self, state):
-        x = F.relu(self.fc1(state))
-        # x = F.relu(self.fc2(x))
-        pi = T.softmax(self.pi(x), dim=1)
+        #change accordingly
+        
+        x = F.relu(self.fc1(state))            #activation function
+        #x = F.relu(self.fc2(x)) ##
+        #x = self.fc2(x)
+        pi = T.softmax(self.pi(x), dim=1)      #hidden layer -> output
 
         return pi
 
@@ -137,3 +182,4 @@ class ActorNetwork(nn.Module):
 
     def load_checkpoint(self):
         self.load_state_dict(T.load(self.load_file))
+
